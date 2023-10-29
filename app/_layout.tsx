64ee5c45d2095861eebee49a8494e0eee9b4ece8
MariuzM <mariusdotdev@gmail.com>
2023-10-29 18:59:35 +0700

    add drawer

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,9 @@ import { Tabs } from 'expo-router/tabs';
 export default function AppLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="settings" />
+      <Tabs.Screen name="index" options={{ title: 'Home' }} />
+      <Tabs.Screen name="about" options={{ title: 'About' }} />
+      <Tabs.Screen name="drawer" options={{ headerShown: false }} />
     </Tabs>
   );
 }
