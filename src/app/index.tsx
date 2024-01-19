7d53fdebd9906137ee045f044f83733f37619797
MariuzM <info@marius.dev>
2024-01-19 23:44:13 +0700

    up

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function Page() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="color-red-500">Home</Text>
+      <Text className="color-blue-500">Home</Text>
     </View>
   );
 }
