545fc89e80dc3ee80875be7fd40ad32feac22fe2
MariuzM <info@marius.dev>
2024-02-12 13:27:47 +0700

    up

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,25 +1,10 @@
-import { Stack } from 'expo-router';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Tabs } from 'expo-router';
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        header(p) {
-          return (
-            <View style={{ backgroundColor: 'lightblue', height: 100, justifyContent: 'flex-end' }}>
-              <Text>TITLE: {p.route.params?.userId}</Text>
-
-              <TouchableOpacity onPress={() => p.navigation.goBack()}>
-                <Text style={{ fontSize: 30 }}> Back to {p.back?.title}</Text>
-              </TouchableOpacity>
-            </View>
-          );
-        },
-      }}
-    >
-      <Stack.Screen name="index" />
-      <Stack.Screen name="[user]" />
-    </Stack>
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="users" />
+    </Tabs>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,29 +1,9 @@
-import { useRouter } from 'expo-router';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function HomePage() {
-  const router = useRouter();
-
-  function onPress(item: string) {
-    router.push('/');
-    router.setParams({ userId: item });
-  }
-
   return (
-    <View
-      style={{
-        alignItems: 'center',
-        flex: 1,
-        justifyContent: 'center',
-      }}
-    >
-      <TouchableOpacity onPress={() => onPress('user1')}>
-        <Text>User 1</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={() => onPress('user2')}>
-        <Text>User 2</Text>
-      </TouchableOpacity>
+    <View style={{ alignItems: 'center', flex: 1, justifyContent: 'center' }}>
+      <Text>Index</Text>
     </View>
   );
 }
