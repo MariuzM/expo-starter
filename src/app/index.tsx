dc4ef9db06bc659f3cd332796f1dd2fcfbd58899
MariuzM <info@marius.dev>
2024-02-16 12:29:18 +0700

    up

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,14 @@
 import { Text, View } from 'react-native';
 
+import Test from './../componentsSecret/_index';
+
 export default function HomePage() {
   return (
-    <View style={{ alignItems: 'center', flex: 1, justifyContent: 'center' }}>
-      <Text>Index</Text>
-    </View>
+    <>
+      <View style={{ alignItems: 'center', flex: 1, justifyContent: 'center' }}>
+        <Text>Index</Text>
+      </View>
+      <Test />
+    </>
   );
 }
