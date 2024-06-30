98f1401a5d0a43a5d892e059a29074fe222c1d20
MariuzM <info@marius.dev>
2024-06-30 23:13:14 +0700

    example

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -8,9 +8,15 @@ export default function RootLayout() {
           headerStyle: {
             backgroundColor: 'blue',
           },
+          contentStyle: {
+            backgroundColor: 'orange',
+          },
           headerTintColor: 'white',
         }}
-      />
+      >
+        <Stack.Screen name="index" />
+        <Stack.Screen name="page1" />
+      </Stack>
     </>
   );
 }
