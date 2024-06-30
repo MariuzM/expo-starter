fa6af3824f13ac0bd6ac926a4c860cd8cf44aeac
MariuzM <info@marius.dev>
2024-06-30 23:06:39 +0700

    up

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,7 +3,14 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <>
-      <Stack />
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: 'blue',
+          },
+          headerTintColor: 'white',
+        }}
+      />
     </>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,15 @@
 import { router } from 'expo-router';
-import { Button } from 'react-native';
+import { Button, View } from 'react-native';
 
 export default function HomePage() {
   return (
-    <>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'red',
+      }}
+    >
       <Button title="Go to Page 1" onPress={() => router.push('/page1')} />
-    </>
+    </View>
   );
 }
